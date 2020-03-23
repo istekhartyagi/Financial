db5db4d134cf9b28d50666a819d0ab7fbdd616f2
istekhar tyagi <istekhartyagi@gmail.com>
2020-03-24 00:09:30 +0530

    first commit

---
+++ src/test/java/com/tyagi/Financial/FinancialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyagi.Financial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
